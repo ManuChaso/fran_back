eb2bb4215ca5bfadf35950d1f418490a36224fb2
ManuChaso <manuc.chaso@gmail.com>
2025-04-10 18:29:44 +0200

    llorando

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -16,21 +16,21 @@ const {
   verificarCodigo
 } = require('../controllers/userController')
 
-router.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173')
-  res.header('Access-Control-Allow-Credentials', 'true')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  )
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+// router.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:5173')
+//   res.header('Access-Control-Allow-Credentials', 'true')
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   )
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
 
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end()
-  }
+//   if (req.method === 'OPTIONS') {
+//     return res.status(200).end()
+//   }
 
-  next()
-})
+//   next()
+// })
 
 router.get('/entrenadores', async (req, res) => {
   try {
